2f6e61b6eb03921026556b8eba78fe69e43604da
ikapyc13 <74313064+ikapyc13@users.noreply.github.com>
2020-11-16 18:30:35 +0200

    товар добавлен 2

--- js/common.js
+++ js/common.js
@@ -32,11 +32,11 @@ jQuery(function($){
 
     $(".latest-products__cart-add-bl-n-size").click(function(){
         $(this).toggleClass("flex-bl");
-        $(this).find("+.latest-products__cart-add-bl-n-akt").toggleClass("flex-bl");
+        //$(this).find("+.latest-products__cart-add-bl-n-akt").toggleClass("flex-bl");
         $('body').append('<div class="cart-add-popup"><div>Товар добавлен </div></div>');
         var x_this = this;
         setTimeout(function() {
-            $(x_this).find("+.latest-products__cart-add-bl-n-akt").toggleClass("flex-bl");
+            //$(x_this).find("+.latest-products__cart-add-bl-n-akt").toggleClass("flex-bl");
             $(".cart-add-popup").remove();
         	$(x_this).parent().find(".latest-products__cart-add").toggleClass("latest-products-off");
         }, 1500);
